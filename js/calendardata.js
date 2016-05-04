cc43592837469fa992b81c82148fc2fb1f4e7618
Juha Lindstedt <juha@pakastin.fi>
2016-05-04 13:39:35 +0300

    fixed indentation

--- js/calendardata.js
+++ js/calendardata.js
@@ -6,40 +6,40 @@ export function createCalendar (year, month, doFixMonday) {
 	const firstDate = new Date(year, month, 1);
 	const lastDate = new Date(year, month + 1, 0)
 
-  // calculate first monday and last sunday
-  const firstMonday = getFirstMonday(firstDate, doFixMonday);
-  const lastSunday = getLastSunday(lastDate, doFixMonday);
+	// calculate first monday and last sunday
+	const firstMonday = getFirstMonday(firstDate, doFixMonday);
+	const lastSunday = getLastSunday(lastDate, doFixMonday);
 
 	// iterate days starting from first monday
 	let iterator = new Date(firstMonday);
-  let i = 0;
+	let i = 0;
 	let week;
 
-  // ..until last sunday
-  while (iterator <= lastSunday) {
+	// ..until last sunday
+	while (iterator <= lastSunday) {
 		if (i++ % 7 === 0) {
-    	// start new week when monday
-    	week = [];
+			// start new week when monday
+			week = [];
 			results.push(week);
 		}
 
-    // push day to week
+		// push day to week
 		week.push({
-    	date: new Date(iterator),
-      before: iterator < firstDate, // add indicator if before current month
-      after: iterator > lastDate // add indicator if after current month
-    });
+			date: new Date(iterator),
+			before: iterator < firstDate, // add indicator if before current month
+			after: iterator > lastDate // add indicator if after current month
+		});
 
 		// iterate to next day
 		iterator.setDate(iterator.getDate() + 1);
 	}
 
-  return results;
+	return results;
 }
 
 function fixMonday (day) {
 	day || (day = 7);
-  return --day;
+	return --day;
 }
 
 function getFirstMonday (firstDate, doFixMonday) {
@@ -48,17 +48,17 @@ function getFirstMonday (firstDate, doFixMonday) {
 	const offset = doFixMonday ? fixMonday(firstDay) : firstDay;
 	const result = new Date(firstDate);
 
-  result.setDate(firstDate.getDate() - offset);
+	result.setDate(firstDate.getDate() - offset);
 
-  return result;
+	return result;
 }
 
 function getLastSunday (lastDate, doFixMonday) {
 	const lastDay = lastDate.getDay();
 	const offset = doFixMonday ? 6 - fixMonday(lastDay) : 6 - lastDay;
-  const result = new Date(lastDate);
+	const result = new Date(lastDate);
 
-  result.setDate(lastDate.getDate() + offset);
+	result.setDate(lastDate.getDate() + offset);
 
 	return result;
 }
